5f3b614db275f0c52dd6fd101d2e44de10e62795
Regis Zaleman <regis.zaleman@gmail.com>
2021-12-15 11:12:11 -0500

    refactor: :recycle: OriginButton extends Container instead of Rectangle
    OriginButton will contain an icon, so it is usefull to extend Container

--- src/classes/OriginButton.ts
+++ src/classes/OriginButton.ts
@@ -1,6 +1,7 @@
 import SiteScene from "~/scenes/SiteScene"
 import { IToggle } from "./IToggle"
-export default class OriginButton extends Phaser.GameObjects.Rectangle implements IToggle {
+export default class OriginButton extends Phaser.GameObjects.Container implements IToggle {
+  public background: Phaser.GameObjects.Rectangle
 
   public constructor(
     scene: Phaser.Scene,
@@ -8,20 +9,53 @@ export default class OriginButton extends Phaser.GameObjects.Rectangle implement
     y: number,
     width: number,
     height: number,
-    backgroundColor: number = 0x333333,
+    backgroundColor: number = 0xFF0000,
     opacity: number = 1
   ) {
-    super(scene, x, y, width, height, backgroundColor, opacity)
-    this.setX(this.x + this.width / 2)
-    this.setY(this.y + this.height / 2)
+    super(scene, x, y)
+    this.setPosition(width / 2, height / 2)
+
+    this.background = new Phaser.GameObjects.Rectangle(
+      this.scene,
+      0,
+      0,
+      width,
+      height,
+      backgroundColor,
+      opacity
+    )
+
+    this.add(this.background)
+
     this.setScrollFactor(0, 0)
-    this.setInteractive({ cursor: 'url(assets/images/curor-hand.png), pointer' })
+    this.setSize(width, height)
+
+    this.setInteractive({
+      hitArea: this.background.geom,
+      cursor: 'pointer'
+    })
+
     this.scene.input.keyboard.on('keydown-R', this.toggle)
+
     this.on('pointerdown', this.clickHandler)
+
+    this.on('pointerup', () => {
+      this.background.setFillStyle(0xCC0000)
+    })
+
+    this.on('pointerover', () => {
+      this.background.setFillStyle(0xCC0000)
+    })
+
+    this.on('pointerout', () => {
+      this.background.setFillStyle(backgroundColor)
+    })
+
     this.scene.add.existing(this)
   }
 
   clickHandler = () => {
+    this.background.setFillStyle(0xFF0000)
     const scene = this.scene as SiteScene
     scene.player.setPosition(0, 0)
   }

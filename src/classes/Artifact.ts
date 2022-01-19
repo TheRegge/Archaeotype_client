90ae0ebdfa1d4857a05ef21238dae6a8e05606c1
Regis Zaleman <regis.zaleman@gmail.com>
2022-01-19 11:12:23 -0500

    feat: :construction: Start implementation of the Artifact class
    Uses `Utils.lazzyLoadImage` to dynamically set the image's texture

--- src/classes/Artifact.ts
+++ src/classes/Artifact.ts
@@ -1,1 +1,42 @@
-export default class Artifact {}
+import Utils from '../common/Utils'
+import Config from '../common/Config'
+
+export type ArtifactData = {
+  id: string
+  mapId: string
+  name: string
+  coordinatesInMeters: { x: number; y: number }
+  imageSizeInPixels: { width: number; height: number }
+  weightInGrams: number
+  heightInCentimeters: number
+  widthCentimeters: number
+  displayAngle: number
+  isPainting: boolean
+  materials: string[]
+  src: string
+  altSrc: string[]
+}
+export default class Artifact extends Phaser.GameObjects.Image {
+  public constructor(scene: Phaser.Scene, artifactData: ArtifactData) {
+    super(
+      scene,
+      Config.WORLD.origin.x +
+        Utils.metersToPixels(artifactData.coordinatesInMeters.x),
+      Config.WORLD.origin.y * 2 +
+        Utils.metersToPixels(artifactData.coordinatesInMeters.y),
+      'artifactPlaceholder'
+    )
+    this.setData(artifactData)
+
+    this.setOrigin(0.5, 0.5)
+    this.setRotation(Utils.degreesToRandian(artifactData.displayAngle))
+    Utils.lazzyLoadImage(
+      this.scene,
+      this,
+      'image',
+      artifactData.src,
+      artifactData.imageSizeInPixels.width,
+      artifactData.imageSizeInPixels.height
+    )
+  }
+}

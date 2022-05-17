2951a26e6b8d4233829756ac8e61477d11522324
Regis Zaleman <regis.zaleman@gmail.com>
2022-05-17 16:50:39 -0400

    feat(COLLECTION): :sparkles: Working Collection for single item

--- src/scenes/subScenes/CollectionItemSubScene.ts
+++ src/scenes/subScenes/CollectionItemSubScene.ts
@@ -10,38 +10,142 @@ export default class extends BaseSubScene {
     this.elements = []
   }
 
-  async initHTML() {
+  initHTML() {
     const { artifactData } = this.data.get('htmlData')
+    const imageContainer = document.querySelector('.zoom') as HTMLElement
+    const img = document.getElementById('zoomImage') as HTMLImageElement
 
-    console.log(artifactData)
+    Data.getFoundArtifact(artifactData.onmap_id).then((result) => {
+      this.data.set('foundArtifact', result[0])
 
-    const result = await Data.getFoundArtifact(artifactData.onmap_id)
-    console.log(result[0]['found_label'])
-
-    this.data.set('foundArtifact', result[0])
-
-    this.elements = [
-      {
-        el: document.getElementById('title') as HTMLInputElement,
-        data: {
-          value: result[0].found_label || '',
-          valueType: 'innerText',
+      this.elements = [
+        {
+          el: document.getElementById('title') as HTMLInputElement,
+          data: {
+            value: result[0].found_label || '',
+            valueType: 'innerText',
+          },
         },
-      },
-    ]
+        {
+          el: document.getElementById('found_by') as HTMLInputElement,
+          data: {
+            value: result[0].username || 'unkowned',
+            valueType: 'value',
+          },
+        },
+        {
+          el: document.getElementById('found_date') as HTMLInputElement,
+          data: {
+            value:
+              new Date(result[0].date).toLocaleDateString('en-us', {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              }) || '',
+            valueType: 'innerText',
+          },
+        },
+        {
+          el: document.getElementById('found_materials') as HTMLInputElement,
+          data: {
+            value: result[0].found_materials || '',
+            valueType: 'innerText',
+          },
+        },
+        {
+          el: document.getElementById('found_notes') as HTMLInputElement,
+          data: {
+            value: result[0].found_notes || '',
+            valueType: 'innerText',
+          },
+        },
+        {
+          el: document.getElementById('found_colors') as HTMLInputElement,
+          data: {
+            value: result[0].found_colors || '',
+            valueType: 'innerText',
+          },
+        },
+        {
+          el: document.getElementById('found_column') as HTMLInputElement,
+          data: {
+            value: result[0].found_column || '',
+            valueType: 'value',
+          },
+        },
+        {
+          el: document.getElementById('found_row') as HTMLInputElement,
+          data: {
+            value: result[0].found_row || '',
+            valueType: 'value',
+          },
+        },
+        {
+          el: document.getElementById('found_width') as HTMLInputElement,
+          data: {
+            value: result[0].found_width || '',
+            valueType: 'value',
+          },
+        },
+        {
+          el: document.getElementById('found_height') as HTMLInputElement,
+          data: {
+            value: result[0].found_height || '',
+            valueType: 'value',
+          },
+        },
+        {
+          el: document.getElementById('found_weight') as HTMLInputElement,
+          data: {
+            value: result[0].found_weight || '',
+            valueType: 'value',
+          },
+        },
+      ]
 
-    this.elements.forEach((element) => {
-      if (element.data) {
-        element.el[element.data.valueType] = element.data.value
-      }
+      this.elements.forEach((element) => {
+        if (element.data) {
+          element.el[element.data.valueType] = element.data.value
+        }
 
-      if (element.action) {
-        this.eventsCancellables.push(element.el)
-        element.el.addEventListener(
-          element.action.event,
-          element.action.callback
-        )
-      }
+        if (element.action) {
+          this.eventsCancellables.push(element.el)
+          element.el.addEventListener(
+            element.action.event,
+            element.action.callback
+          )
+        }
+      })
+
+      img.src = `${process.env.API_URL}resource/artifacts/preview/${artifactData.fileName}.png`
+      imageContainer.onmousemove = (event) => zoom(event)
+      imageContainer.onmouseout = () => stopZoom()
     })
+
+    const stopZoom = () => {
+      imageContainer.style.backgroundImage = 'none'
+    }
+
+    const zoom = (e: MouseEvent | TouchEvent) => {
+      imageContainer.style.backgroundImage = `url("${process.env.API_URL}resource/artifacts/full/${artifactData.fileName}.png")`
+
+      let imageZoom = e.currentTarget as HTMLDivElement
+      let offsetX
+      let offsetY
+
+      if (e instanceof MouseEvent) {
+        offsetX = e.offsetX
+        offsetY = e.offsetY
+      } else if (e instanceof TouchEvent) {
+        offsetX = e.touches[0].pageX
+        offsetY = e.touches[0].pageY
+      }
+
+      let x = (offsetX / imageZoom.offsetWidth) * 100
+      let y = (offsetY / imageZoom.offsetHeight) * 100
+
+      imageZoom.style.backgroundPosition = `${x}% ${y}%`
+    }
   }
 }

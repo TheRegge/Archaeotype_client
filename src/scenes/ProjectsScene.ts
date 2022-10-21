f8ab9839dfdcba321480fa7ae53e00afb9d44642
Regis Zaleman <regis.zaleman@gmail.com>
2022-10-21 13:10:11 -0400

    fix: :bug: Fix display of projects and scenes

--- src/scenes/ProjectsScene.ts
+++ src/scenes/ProjectsScene.ts
@@ -11,13 +11,17 @@ export default class ProjectsScene extends BaseScene {
 
   constructor() {
     super({ key: 'projects' })
-    this.lineSpacing = 40
+    this.lineSpacing = 36
   }
 
   create() {
     this.transitionIn()
 
-    this.add.text(100, 100, 'Projects')
+    this.add.text(100, 100, 'Archaeotype Projects', {
+      fontSize: '36px',
+      fontFamily: config.GOOGLE_FONT_FAMILY,
+      color: Utils.hexToString(config.COLOR_HINT_SECONDARY),
+    })
     axios
       .get(`${process.env.API_URL}/project`, {
         headers: {
@@ -27,22 +31,28 @@ export default class ProjectsScene extends BaseScene {
       .then((res) => {
         let y = 100 + this.lineSpacing
         res.data.projects.map((project) => {
+          y += this.lineSpacing
           const content = project.title + ' - ' + project.description
-          this.add.text(100, y, content, {
-            color: Utils.hexToString(config.COLOR_HINT_PRIMARY),
-          })
 
+          this.add.text(100, y, content, {
+            color: Utils.hexToString(config.COLOR_GRAY_100),
+            fontFamily: config.GOOGLE_FONT_FAMILY,
+            fontSize: '16px',
+          })
+          y += this.lineSpacing / 2
           project.sites.map((site) => {
             y += this.lineSpacing
-            this.makeSiteLink(project.id, site, y)
+            this.makeSiteLink(project.id, site, Math.ceil(y))
           })
         })
       })
   }
 
   makeSiteLink = (projectId: string, site: any, offset: number): void => {
-    const text = this.add.text(120, offset, `* ${site.name}`, {
+    const text = this.add.text(100, offset, `* ${site.name}`, {
       color: Utils.hexToString(config.COLOR_HINT_PRIMARY),
+      fontFamily: config.GOOGLE_FONT_FAMILY,
+      fontSize: '14px',
     })
 
     text.setInteractive({
